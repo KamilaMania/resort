d68802077807e13ac78fd93ad995b5ad3a9361ae
KamilaMania <mania.kamika@icloud.com>
2020-05-28 14:54:14 +0200

    filters added

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import defaultBcg from "../images/room-1.jpeg";
-// import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
@@ -9,14 +8,12 @@ import StyledHero from "../components/StyledHero";
 export default class SingleRoom extends Component {
   constructor(props) {
     super(props);
-    // console.log(this.props);
     this.state = {
       slug: this.props.match.params.slug,
       defaultBcg,
     };
   }
   static contextType = RoomContext;
-  // componentDidMount() {}
   render() {
     const { getRoom } = this.context;
     const room = getRoom(this.state.slug);
